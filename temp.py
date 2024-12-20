0d7d91bf985c2d9c525de16ed73e3683e298141d
dumdumvillhatuggum <142681029+dumdumvillhatuggum@users.noreply.github.com>
2024-12-20 21:57:26 +0100

    custom time and threads for sysbench

--- temp.py
+++ temp.py
@@ -1,16 +1,24 @@
 import argparse
 import subprocess
+import multiprocessing
 from gpiozero import CPUTemperature
 from time import sleep, strftime, time
 
-STRESS_COMMAND = ["sysbench", "cpu", "run", "--threads=4", "--time=10"]
+MACHINE_THREADS = multiprocessing.cpu_count()
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--timestamp', action='store_true')
+parser.add_argument('-d', '--date', action='store_true')
 parser.add_argument('-v', '--verbose', action='store_true')
 parser.add_argument('-f', '--filename', action='store', default="cputemp.csv")
 parser.add_argument('-p', '--path', action='store', default="")
+parser.add_argument('-t', '--threads', action='store', default=str(multiprocessing.cpu_count()))
+parser.add_argument('-s', '--seconds', action='store', default="10")
 args = parser.parse_args()
+
+STRESS_COMMAND = ["sysbench", "cpu", "run", 
+"--threads=" + args.threads, 
+"--time=" + args.seconds
+]
 
 tmp = ""
 if(args.path):
@@ -18,19 +26,17 @@ if(args.path):
         tmp = "/"
 FILE = args.path + tmp + args.filename
 
-print("FILE: ", FILE)
-
 if(args.verbose):
     print("Program started at " + strftime("%Y-%m-%d %H:%M:%S"))
     print("Arguments:")
-    print("timestamp:", args.timestamp)
+    print("date:", args.date)
     print("verbose:", args.verbose)
     print("file: ", args.file)
     print()
 
 def write_temp(temp):
     with open("cputemp.csv", "a") as log:
-        if args.timestamp:
+        if args.date:
             lineToWrite = "{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp))
         else:
             lineToWrite = "{0}\n".format(str(temp))
@@ -39,7 +45,7 @@ def write_temp(temp):
 
 write_temp(69)
 
-ls = subprocess.run(STRESS_COMMAND, shell=True, capture_output=True, text=True)
+ls = subprocess.run(STRESS_COMMAND, capture_output=True, text=True)
 print(ls.stdout)
 
 #while True:
