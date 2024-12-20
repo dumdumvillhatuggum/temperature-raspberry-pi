a57a366b0fe2f25d99ec82acc4e297fd7e4ab3a8
dumdumvillhatuggum <142681029+dumdumvillhatuggum@users.noreply.github.com>
2024-12-20 19:33:15 +0100

    move temperature csv to local directory and +x permissions

--- temp.py
+++ temp.py
@@ -1,10 +1,11 @@
+import sys
 from gpiozero import CPUTemperature
 from time import sleep, strftime, time
 
 cpu = CPUTemperature()
 
 def write_temp(temp):
-    with open("/home/huser/my-fanshim/templog/cputemp.csv", "a") as log:
+    with open("cputemp.csv", "a") as log:
         log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
 
 while True:
