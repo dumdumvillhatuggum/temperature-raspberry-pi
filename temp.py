8dd3262ea2484a59629687d050d34f55053b2027
dumdumvillhatuggum <142681029+dumdumvillhatuggum@users.noreply.github.com>
2024-12-20 22:14:52 +0100

    sdf

--- temp.py
+++ temp.py
@@ -7,10 +7,42 @@ parser.add_argument('-t', '--timestamp', action='store_true')
 #parser.add_argument('-o', '--output-dir')
 args = parser.parse_args('-t'.split())
 
+<<<<<<< Updated upstream
 if(args.timestamp):
     print("flag recognozed")
 else:
     print("no flag")
+=======
+STRESS_COMMAND = ["sysbench", "cpu", "run", 
+"--threads=" + args.threads, 
+"--time=" + args.seconds
+]
+
+tmp = ""
+if(args.path):
+    if(args.path[-1] != "/"):
+        tmp = "/"
+FILE = args.path + tmp + args.filename
+
+if(args.verbose):
+    print("Program started at " + strftime("%Y-%m-%d %H:%M:%S"))
+    print("Arguments:")
+    print("date:", args.date)
+    print("verbose:", args.verbose)
+    print("file: ", args.file)
+    print()
+
+print("Start stress")
+def write_temp(temp):
+    with open("cputemp.csv", "a") as log:
+        if args.date:
+            lineToWrite = "{0},{1},{2}\n".format(strftime("%Y-%m-%d %H:%M:%S"),"      ",str(temp))
+        else:
+            lineToWrite = "{0}\n".format(str(temp))
+        log.write(lineToWrite)
+        # TODO if verbose print temp
+print("End stress")
+>>>>>>> Stashed changes
 
 cpu = CPUTemperature()
 
